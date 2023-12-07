8686da62b67c6baa2ef3e7fea57bfa720804d7d3
Devex <153024010+DevexGit@users.noreply.github.com>
2023-12-07 21:36:11 +0100

    Add files via upload

---
+++ Dialogue.py
@@ -0,0 +1,4 @@
+name = input("Hello, what is your name? \nInsert Name Here: ")
+print("Nice to meet you, " + name)
+insert_question = input("Did you know i made this program of boredom? \nInsert Answer Here: ")
+print("well, i suppose you said " + insert_question)
